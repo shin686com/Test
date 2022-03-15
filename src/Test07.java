641834e1c485449d2719fe5ae5b27ef6d45c28a5
shin686com <sgs686@naver.com>
2022-03-15 14:22:26 +0900

    20220315_Test06

---
+++ src/Test07.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Test07 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int A = input.nextInt();
+		int B = input.nextInt();
+		if(A>B) {
+			System.out.println(">");
+		}else if(A<B) {
+			System.out.println("<");
+		}else if(A==B){
+			System.out.println("==");
+		}
+	}
+}
